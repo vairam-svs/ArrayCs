f610f590fe31d130abd3c083386ef42c7ba7a6eb
vairam-svs <vairam.svs@outlook.com>
2015-04-02 12:04:31 -0700

    converted to use array and removed cloned code.
    converted to use array and removed cloned code.

--- README.md
+++ README.md
@@ -5,3 +5,5 @@ Change History:
 Version 0.0.1: added string array extension to split at index provided of a given string.
 
 Version 0.0.2: converted to split at intervals instead of just only once. if required to do only once, set bool to false.
+
+Version 0.0.3: converted to use array and removed cloned code.
--- src/ArrayCs/StringArray.cs
+++ src/ArrayCs/StringArray.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Runtime.InteropServices;
-using System.Collections.Generic;
+
+//using System.Collections.Generic;
 
 namespace ArrayCs
 {
@@ -18,29 +19,35 @@ namespace ArrayCs
         public static string[] Split(this string inputValue, int splitInterval, bool splitAtIntervals=true, StringSplitOptions options = StringSplitOptions.None)
         {
             if (inputValue == null) throw new ArgumentNullException("inputValue");
+
             if (splitInterval < 0) throw new ArgumentOutOfRangeException("splitAt", splitInterval, "splitAt cannot be less than 0");
 
             if (options == StringSplitOptions.RemoveEmptyEntries && inputValue.Trim().Length == 0)
                 return null;
+
             if (splitInterval == 0)
                 return new string[] { inputValue };
+
             //if there is only one item 
             if (options == StringSplitOptions.RemoveEmptyEntries && string.IsNullOrEmpty(inputValue.Substring(splitInterval).Trim()))
                 return new string[] { inputValue.Substring(0, splitInterval) };
+
             //if to split only once.
             if (splitAtIntervals == false)
                 return new string[] { inputValue.Substring(0, splitInterval), inputValue.Substring(splitInterval) };
-            List<string> stringList = new List<string>() { inputValue.Substring(0, splitInterval) };
-            inputValue = inputValue.Substring(splitInterval);
+
+            string[] stringList = new string[(int)Math.Ceiling(inputValue.Length / (decimal)splitInterval)];
+            //start
+            int stringListIndex = 0;
             while(!string.IsNullOrEmpty(inputValue))
             {
-                stringList.Add(inputValue.Substring(0, (splitInterval < inputValue.Length ? splitInterval : inputValue.Length)));
+                stringList[stringListIndex++] = inputValue.Substring(0, (splitInterval < inputValue.Length ? splitInterval : inputValue.Length));
                 if (splitInterval < inputValue.Length)
                     inputValue = inputValue.Substring(splitInterval);
                 else
                     inputValue = string.Empty;
             }
-            return stringList.ToArray();
+            return stringList;
         }
     }
 }
