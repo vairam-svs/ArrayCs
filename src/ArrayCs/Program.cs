a6cfab7d0e5e08a696d49a251009a2a892564833
vairam-svs <vairam.svs@outlook.com>
2015-04-02 10:22:22 -0700

    added string array extension  to split at index provided.
    added string array extension  to split at index provided.

---
+++ src/ArrayCs/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace ArrayCs
+{
+    public class Program
+    {
+        public void Main(string[] args)
+        {
+            string writeLine = "Hello World";
+            bool doYouWantToContinue = true;
+            while (doYouWantToContinue)
+            {
+                Console.WriteLine("Enter a string to split at length 6");
+                writeLine = Console.ReadLine();
+                Console.WriteLine("Input String {0} split into array at length 6 is as follows", writeLine);
+                Action continueProgram = () =>{
+                    Console.WriteLine("Do you want to continue");
+                    string continueValue = Console.ReadLine().ToString().Trim().ToUpperInvariant();
+                    doYouWantToContinue = (continueValue == "Y");
+                    return;
+                };
+                try
+                {
+                    string[] arrayOfString = writeLine.Split(6);
+                    foreach (string stringvalueAt in arrayOfString)
+                    {
+                        Console.WriteLine(stringvalueAt);
+                    }
+                    continueProgram();
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine(e.Message);
+                    continueProgram();
+                }
+            }
+        }
+    }
+}
---
+++ src/ArrayCs/StringArray.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Runtime.InteropServices;
+
+namespace ArrayCs
+{
+    public static class StringArray
+    {
+        /// <summary>
+        /// This is an string extension to split string into array at position index provided.
+        /// </summary>
+        /// <param name="inputValue"></param>
+        /// <param name="splitAt"></param>
+        /// <param name="options"></param>
+        /// <returns>string array</returns>
+        [ComVisibleAttribute(false)]
+        public static string[] Split(this string inputValue, int splitAt, StringSplitOptions options = StringSplitOptions.None)
+        {
+            if (inputValue == null) throw new ArgumentNullException("inputValue");
+            if (splitAt < 0) throw new ArgumentOutOfRangeException("splitAt", splitAt, "splitAt cannot be less than 0");
+
+            if (options == StringSplitOptions.RemoveEmptyEntries && inputValue.Trim().Length == 0)
+                return null;
+            if (splitAt == 0)
+                return new string[] { inputValue};
+            if (options == StringSplitOptions.RemoveEmptyEntries && string.IsNullOrEmpty(inputValue.Substring(splitAt).Trim()))
+                return new string[] { inputValue.Substring(0, splitAt) };
+            return new string[] { inputValue.Substring(0, splitAt), inputValue.Substring(splitAt) };
+        }
+    }
+}
